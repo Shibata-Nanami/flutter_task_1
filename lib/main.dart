a8f721d63adf3a3bcb723b12cd6fd3e628b05b59
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-07 19:14:28 +0900

    表示領域設定と余白修正

--- lib/main.dart
+++ lib/main.dart
@@ -17,60 +17,67 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 90),
-        child: Column(
-          children: [
-            Container(
-              height: 90,
-              width: 260,
-              color: Colors.black,
-            ),
-            GridView.builder(
-              padding: const EdgeInsets.all(25),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, //カラム数
-                crossAxisSpacing: 20,
-                // Widget間のスペース（上下）
-                mainAxisSpacing: 20,
-                // 全体の余白
-              ),
-              itemCount: 6, //要素数
-              itemBuilder: (context, index) {
-                //要素を戻り値で返す
-                return Container(
-                  color: Colors.blue,
-                );
-              },
-              shrinkWrap: true,
-            ),
-            Column(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: SingleChildScrollView(
+            child: Column(
               children: [
                 Container(
                   height: 90,
                   width: 260,
                   color: Colors.black,
                 ),
-                SizedBox(
-                  width: 360,
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        return const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10.0),
-                          child: SizedBox(
-                            height: 80,
-                            child: Card(
-                              color: Colors.yellow,
-                            ),
-                          ),
-                        );
-                      }),
+                GridView.builder(
+                  padding: const EdgeInsets.all(25),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3, //カラム数
+                    crossAxisSpacing: 20,
+                    // Widget間のスペース（上下）
+                    mainAxisSpacing: 20,
+                    // 全体の余白
+                  ),
+                  itemCount: 6, //要素数
+                  itemBuilder: (context, index) {
+                    //要素を戻り値で返す
+                    return Container(
+                      color: Colors.blue,
+                    );
+                  },
+                  shrinkWrap: true,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: 90,
+                      width: 260,
+                      color: Colors.black,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: SizedBox(
+                        width: 360,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: 3,
+                            itemBuilder: (context, index) {
+                              return const Padding(
+                                padding: EdgeInsets.only(bottom: 20),
+                                child: SizedBox(
+                                  height: 80,
+                                  child: Card(
+                                    color: Colors.yellow,
+                                  ),
+                                ),
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
