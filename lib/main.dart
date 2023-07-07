803814080e1649e3c06ab7fe3f5a05401419141e
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-07 18:45:10 +0900

    位置修正（黄色部分以外）

--- lib/main.dart
+++ lib/main.dart
@@ -17,59 +17,61 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Container(
-            height: 90,
-            width: 260,
-            color: Colors.black,
-          ),
-          GridView.builder(
-            padding: const EdgeInsets.all(15),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3, //カラム数
-              crossAxisSpacing: 20,
-              // Widget間のスペース（上下）
-              mainAxisSpacing: 20,
-              // 全体の余白
+      body: Padding(
+        padding: const EdgeInsets.only(top: 90),
+        child: Column(
+          children: [
+            Container(
+              height: 90,
+              width: 260,
+              color: Colors.black,
             ),
-            itemCount: 6, //要素数
-            itemBuilder: (context, index) {
-              //要素を戻り値で返す
-              return Container(
-                color: Colors.blue,
-              );
-            },
-            shrinkWrap: true,
-          ),
-          Column(
-            children: [
-              Container(
-                height: 90,
-                width: 260,
-                color: Colors.black,
+            GridView.builder(
+              padding: const EdgeInsets.all(25),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, //カラム数
+                crossAxisSpacing: 20,
+                // Widget間のスペース（上下）
+                mainAxisSpacing: 20,
+                // 全体の余白
               ),
-              SizedBox(
-                width: 360,
-                child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: 3,
-                    itemBuilder: (context, index) {
-                      return const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10.0),
-                        child: SizedBox(
-                          height: 80,
-                          child: Card(
-                            color: Colors.yellow,
+              itemCount: 6, //要素数
+              itemBuilder: (context, index) {
+                //要素を戻り値で返す
+                return Container(
+                  color: Colors.blue,
+                );
+              },
+              shrinkWrap: true,
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 90,
+                  width: 260,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  width: 360,
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: 3,
+                      itemBuilder: (context, index) {
+                        return const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10.0),
+                          child: SizedBox(
+                            height: 80,
+                            child: Card(
+                              color: Colors.yellow,
+                            ),
                           ),
-                        ),
-                      );
-                    }),
-              ),
-            ],
-          ),
-        ],
+                        );
+                      }),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
